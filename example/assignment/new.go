5fc0dc18aaf1037e56d45a8d43c265341e60e3cb
wangyi6 <wangyi6@joyy.com>
2024-05-18 13:19:50 +0800

    1. 调整报错

--- example/assignment/new.go
+++ example/assignment/new.go
@@ -12,8 +12,8 @@ var (
 // NewAssignmentTest1 使用 new进行赋值，new得到的是指针
 func NewAssignmentTest1() {
 	// 这句可以mock
-	receiverResult := newDr.TestReceiverFunc(configKey)
-	if receiverResult == configKey {
+	receiverResult := newDr.TestReceiverFunc(varConfigKey)
+	if receiverResult == varConfigKey {
 		fmt.Print("len(configKey) > 10")
 	}
 }
