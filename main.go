ced3794495b3f6908e7a01c47fff8fa2ed91eeba
wangyi6 <wangyi6@joyy.com>
2024-05-19 22:55:08 +0800

    1. 使用var赋值

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "caseGenerator/parse"
 
 func main() {
 	//err := parse.Extract("./example/type_assertion.go")
-	err := parse.Extract("./example/assignment/var.go")
+	//err := parse.Extract("./example/assignment/var.go")
+	err := parse.Extract("./example/assignment/new.go")
 	if err != nil {
 		return
 	}
