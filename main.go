029245ca8ff9d8c5db2bc4cbb965ff0aee32c7a0
wangyi6 <wangyi6@joyy.com>
2024-05-28 23:36:17 +0800

    解析多个文件目录

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import "caseGenerator/parse"
 
 func main() {
-	//err := parse.Extract("./example/type_assertion.go")
-	//err := parse.Extract("./example/assignment/var.go")
-	//err := parse.Extract("./example/assignment/new.go")
-	err := parse.Extract("./example/assignment/")
+	err := parse.Extract("./example/type_assertion.go")
+	err1 := parse.Extract("./example/assignment/var.go")
+	err2 := parse.Extract("./example/assignment/new.go")
+	err3 := parse.Extract("./example/assignment/")
 	if err != nil {
+		return
+	}
+	if err1 != nil {
+		return
+	}
+	if err2 != nil {
+		return
+	}
+	if err3 != nil {
 		return
 	}
 }
