0dd46b724a669f4d48c6510981c094bc059ee2ef
wangyi6 <wangyi6@joyy.com>
2024-06-12 22:12:03 +0800

    开始开发go:link逻辑

--- parse/parse.go
+++ parse/parse.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func Extract(path string, excludedPaths ...string) error {
@@ -228,142 +229,41 @@ func ExtractGoLinkFunction(path string, excludedPaths ...string) error {
 	})
 }
 
-// 解析文件，并且生成go-linkname的方法
-func parseLinkFile(filename string) error {
+// 遍历文件夹，找到所有私有方法的调用
+func extractPrivateFile(filename string) (err error) {
 	// 1. 遍历解析文件夹
-
-}
-func extractFile(filename string) (err error) {
-	defer func() {
-		if err := recover(); err != nil {
-			_ = fmt.Errorf("extractFile parse error: %s", err)
+	err = filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+		// Process error
+		if err != nil {
+			return err
 		}
-	}()
-	// Parse file and create the AST
-	var fset = token.NewFileSet()
-	var f *ast.File
-	if f, err = parser.ParseFile(fset, filename, nil, parser.ParseComments); err != nil {
-		return
-	}
 
-	// 1. 组装package、method信息
-	_ = bo.Package{PackagePath: "", PackageName: f.Name.Name}
-	// 2. 组装import信息
-	bo.InitImport(f)
-	// 组装所有方法
-	methods := make([]string, 0, 10)
-	caseDetailList := make([]generate.CaseDetail, 0, 10)
-	// Loop in comment groups
-OuterLoop:
-	for _, cg := range f.Decls {
-		// 1. 先清理bo
-		bo.ClearBo()
-		decl, ok := cg.(*ast.FuncDecl)
-		if !ok {
-			fmt.Print("不处理非 ast.FuncDecl 的内容")
-			continue
-		}
-		// 跳过某些方法的设置
-		if decl.Doc != nil {
-			for _, com := range decl.Doc.List {
-				// 跳过处理
-				if strings.Contains(com.Text, "skipcodeautoparse") {
-					fmt.Print("skip auto parse...")
-					continue OuterLoop
+		// Only process go files
+		if !info.IsDir() && filepath.Ext(path) == ".go" {
+			// Parse the file
+			fset := token.NewFileSet()
+			file, err := parser.ParseFile(fset, path, nil, parser.AllErrors)
+			if err != nil {
+				return err
+			}
+
+			// Traverse the AST
+			ast.Inspect(file, func(node ast.Node) bool {
+				// Check if the node is a function declaration
+				funcDecl, ok := node.(*ast.FuncDecl)
+				if !ok {
+					return true
 				}
-			}
-		}
 
-		methods = append(methods, decl.Name.Name)
-		// 3. 组装method方法
-		methodInfo := bo.Method{MethodName: decl.Name.Name}
-		fmt.Print("\n开始处理:" + decl.Name.Name)
-		// 4. walk所有receiver.
-		var receiverVisitor vistitor.ReceiverVisitor
-		ast.Walk(&receiverVisitor, cg)
-		// 5. 获取typeParam
-		var typeParamVisitor vistitor.TypeParamVisitor
-		ast.Walk(&typeParamVisitor, cg)
-		// 6. 定义所有request
-		vistitor.ParseRequest(decl.Type.Params)
-		// 7. 判断是否有类型断言
-		var typeAssertVisitor vistitor.TypeAssertionVisitor
-		ast.Walk(&typeAssertVisitor, cg)
-		combinationSLice := typeAssertVisitor.CombinationTypeAssertionRequest(bo.GetRequestDetailList())
-		for i, v := range combinationSLice {
-			cd := generate.CaseDetail{
-				CaseName:    methodInfo.MethodName + strconv.Itoa(i),
-				MethodName:  methodInfo.MethodName,
-				RequestList: v,
-			}
-			caseDetailList = append(caseDetailList, cd)
-		}
-
-		// 8. 获取所有赋值、变量
-		var assignmentVisitor vistitor.AssignmentVisitor
-		ast.Walk(&assignmentVisitor, cg)
-		// 8.1 遍历所有赋值和变量
-		list := bo.GetAssignmentDetailInfoList()
-		//marshal, err := json.Marshal(list)
-		//if err != nil {
-		//	fmt.Printf("遍历所有赋值失败，详情为:%s", err.Error())
-		//
-		//}
-		//fmt.Printf("遍历所有赋值为:%+v", string(marshal))
-		for _, v := range list {
-			//if v.RightType == enum.RIGHT_TYPE_CALL {
-			// mockey.Mock((*repo.ClearingPipeConfigRepo).GetAllConfigs).Return(clearingPipeConfigs).Build()
-			// 	"github.com/bytedance/mockey"
-			bo.AppendImportList("bytedanceMockey \"github.com/bytedance/mockey\"")
-			bo.AppendMockInfoList(generate.MockInstruct{
-				MockResponseParam:  v.LeftName,
-				MockFunction:       v.RightFormula,
-				MockFunctionParam:  nil,
-				MockFunctionResult: nil,
+				// 判断是否是私有方法
+				if unicode.IsLower([]rune(funcDecl.Name.Name)[0]) {
+					// 解析私有方法
+					fmt.Println(funcDecl.Name.Name)
+				}
+				return true
 			})
-			//}
 		}
-
-		// 10. 开始处理receiver
-		cd := generate.CaseDetail{
-			CaseName:    methodInfo.MethodName,
-			MethodName:  methodInfo.MethodName,
-			RequestList: bo.GetRequestDetailList(),
-			MockList:    bo.GetMockInfoList(),
-		}
-		if bo.GetReceiverInfo() != nil {
-			cd.ReceiverType = "utils.Empty[" + bo.GetReceiverInfo().ReceiverValue.InvocationName + "]()"
-			bo.AppendImportList("\"caseGenerator/utils\"")
-		}
-		// 11. 开始处理mock
-
-		// 12. 处理import
-		caseDetailList = append(caseDetailList, cd)
-
-	}
-	packageName := f.Name.Name
-	// ./core/service/complex_conditon_service.go，去掉前缀和后缀
-	newFilename := strings.Replace(filename, "./", "", 1)
-	newFilename = strings.TrimSuffix(newFilename, ".go")
-	// 获取最后一个斜杠的位置
-	lastIndex := strings.LastIndex(newFilename, "/")
-	// 如果找到了斜杠
-	if lastIndex == -1 {
-		log.Fatalf("路径存在问题")
-	}
-	// 分割字符串
-	filePath := newFilename[:lastIndex+1]
-	fileName := newFilename[lastIndex+1:]
-
-	gm := generate.GenMeta{
-		Package:        packageName,
-		FileName:       fileName,
-		FilePath:       filePath,
-		ImportPkgPaths: bo.GetImportInfo(),
-		CaseDetailList: caseDetailList,
-	}
-
-	generate.GenFile(gm)
-
-	return
+		return nil
+	})
+	return err
 }
