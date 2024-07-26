b1527b08ce3410076a28e7b5afd61eaa73ee4708
wangyi6 <wangyi6@joyy.com>
2024-07-26 23:09:56 +0800

    1. 整体解析sourceInfo，解析package、file、condition、assignment

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"caseGenerator/common/utils"
-	"caseGenerator/parse"
+	"caseGenerator/parser"
 	"fmt"
+	"log"
+	"os/exec"
 )
 
 func main() {
-	err := parse.Extract("./example/assignment/")
+	cmd := exec.Command("ls", "-l", "/var/log/")
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return
+		fmt.Printf("combined out:\n%s\n", string(out))
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	modulePath := utils.GetModulePath()
-	fmt.Print(modulePath)
+	fmt.Printf("combined out:\n%s\n", string(out))
+	// 1. 解析全部ast字节
+	sourceInfo := parser.SourceInfo{}
+	sourceInfo.ParseSource("")
+	// 2. 根据sourceInfo
+
+	// 3. 解析模板
 
 }
