23f0c38cd6e9215d629ed40cf98695daefeb19b9
wangyi6 <wangyi6@joyy.com>
2024-08-25 18:02:47 +0800

    1. 整体解析sourceInfo，解析package、file、condition、assignment

--- generate/request.go
+++ generate/request.go
@@ -1,22 +1,28 @@
 package generate
 
-// Request 记录方法要请求的信息
+// Request 记录要生成的请求信息
 type Request struct {
-	// receiver相关
+	RequestCaseList []RequestCase
+	// ReceiverName  代表就是单测中receiver的名字，默认是`convert`
 	ReceiverName string
+	// ReceiverType 代表就是单测中receiver的类型，比如说是`某个struct`、`int`等
 	ReceiverType string
-	// 参数
-	ParamList []Param
+	// ReceiverValue 代表了单测中要设置的值，默认是用`lo.Empty[int]`
+	ReceiverValue string
 }
 
-type Param struct {
-	ParamDetail ParamDetail
+// RequestCase 可能一个请求想有多种返回，比如说想给请求设置lo.Empty[]、又想给请求设置fakeit、又想自定义一些字段，这是可以有多个RequestCase
+type RequestCase struct {
+	RequestList       []RequestDetail
+	RequestNameString string
 }
-type ParamDetail struct {
-	ParamName string
-	ParamType string
-	//递归子类型
-	ChildParamList *ParamDetail
+
+type RequestDetail struct {
+	Name  string
+	Value string
 }
 
 // 记录receiver，方法名，请求参数
+//func ()  {
+
+//}
