56a5549dcf88985a37a49e2e3cf0d4ff61b94866
wangyi6 <wangyi6@joyy.com>
2024-08-27 21:16:34 +0800

    1. 整体解析sourceInfo，解析package、file、condition、assignment

---
+++ common/constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	RECEIVER_NAME = "converted"
+)
--- generate/request.go
+++ generate/request.go
@@ -1,5 +1,13 @@
 package generate
 
+import (
+	"caseGenerator/common/constants"
+	"caseGenerator/common/enum"
+	"caseGenerator/common/utils"
+	"caseGenerator/parser"
+	"strings"
+)
+
 // Request 记录要生成的请求信息
 type Request struct {
 	RequestCaseList []RequestCase
@@ -23,6 +31,50 @@ type RequestDetail struct {
 }
 
 // 记录receiver，方法名，请求参数
-//func ()  {
-
-//}
+func (r *Request) generateRequest(si *parser.SourceInfo) {
+	// 1. 组装receiver
+	receiver := si.FunctionDeclare.Receiver
+	r.ReceiverName = constants.RECEIVER_NAME
+	r.ReceiverType = receiver.Type
+	r.ReceiverValue = utils.LoEmpty(receiver.Type)
+	// 2. 组装lo.Empty[]的请求参数
+	empty := RequestCase{
+		RequestList: make([]RequestDetail, 0, 10),
+	}
+	var emptyRequestNameString string
+	for _, v := range si.RequestList {
+		empty.RequestList = append(empty.RequestList, RequestDetail{
+			Name:  v.Name,
+			Value: utils.LoEmpty(v.Type),
+		})
+		if v.AstType == enum.PARAM_AST_TYPE_Ellipsis {
+			emptyRequestNameString += "tt.args." + v.Name + "... , "
+		} else {
+			emptyRequestNameString += "tt.args." + v.Name + ", "
+		}
+	}
+	emptyRequestNameString = strings.TrimRight(emptyRequestNameString, ", ")
+	empty.RequestNameString = emptyRequestNameString
+	// 3. 组装fakeit的请求参数
+	fakeit := RequestCase{
+		RequestList: make([]RequestDetail, 0, 10),
+	}
+	var fakeitRequestNameString string
+	for _, v := range si.RequestList {
+		fakeit.RequestList = append(empty.RequestList, RequestDetail{
+			Name:  v.Name,
+			Value: "gofakeit.Struct(" + v.Type + ")",
+		})
+		if v.AstType == enum.PARAM_AST_TYPE_Ellipsis {
+			fakeitRequestNameString += "tt.args." + v.Name + "... , "
+		} else {
+			fakeitRequestNameString += "tt.args." + v.Name + ", "
+		}
+	}
+	fakeitRequestNameString = strings.TrimRight(fakeitRequestNameString, ", ")
+	fakeit.RequestNameString = fakeitRequestNameString
+	// 4. 暂时只有这两种类型，直接组装为request列表
+	caseList := make([]RequestCase, 0, 10)
+	caseList = append(caseList, empty)
+	caseList = append(caseList, fakeit)
+}
