109772dfd80a323591bcc957a6440813b06f869a
wangyi6 <wangyi6@shopline.com>
2025-01-09 23:58:04 +0800

    1. if-condition 调通

--- parser/stmt/if_test.go
+++ parser/stmt/if_test.go
@@ -216,11 +216,6 @@ func TestParseBlockCase(t *testing.T) {
 // 2. a>3 && b >0 && a ==5
 // 3. a <= 3 && b > 20
 // 4. a <= 3 && b <= 20
-//
-//	else if a > 5 {
-//					fmt.Println("a > 5 && b > 1111")
-//					return
-//				}
 func TestParseSimpleBlockCase(t *testing.T) {
 	sourceCode := `
     package main
