f08adab20efea280b56d0ceadc6a50790bdcf5f2
wangyi6 <wangyi6@shopline.com>
2025-01-29 23:04:11 +0800

    1. 解析嵌套的 switch-condition

--- parser/stmt/switch_test.go
+++ parser/stmt/switch_test.go
@@ -49,6 +49,57 @@ func TestSwitchCase(t *testing.T) {
 	})
 }
 
+func TestDuplicateSwitchCase(t *testing.T) {
+	sourceCode := `
+       package main
+    func main() {
+        x := 2
+    // 使用初始化表达式
+    switch y := x * 2; y {
+    case 2:
+		switch y := y * 2; y {
+		case 2:
+        	fmt.Println("y is 2")
+		case 4:
+        	fmt.Println("y is 4")
+    	case 6:
+        	fmt.Println("y is 6")
+    	default:
+        	fmt.Println("y is some other value")
+    	}
+        fmt.Println("y is 2")
+    case 4:
+        fmt.Println("y is 4")
+    case 6:
+        fmt.Println("y is 6")
+    default:
+        fmt.Println("y is some other value")
+    }
+    }
+    `
+	fset := token.NewFileSet()
+	// 解析代码
+	file, err := parser.ParseFile(fset, "", sourceCode, 0)
+	if err != nil {
+		fmt.Println("解析出错:", err)
+		return
+	}
+	// 遍历函数体中的语句
+	// 遍历 AST
+	ast.Inspect(file, func(n ast.Node) bool {
+		if stmt, ok := n.(*ast.SwitchStmt); ok {
+			fmt.Println("找到 SwitchStmt")
+			parseSwitch := ParseSwitch(stmt)
+			// 被断言的对象
+			fmt.Printf("接口对象: %v\n", parseSwitch)
+			// 解析 condition
+			conditionResult := parseSwitch.ParseSwitchCondition()
+			fmt.Printf(" 解析condition后的结果: %v\n", conditionResult)
+		}
+		return true
+	})
+}
+
 func TestParseIfProblemCase(t *testing.T) {
 	sourceCode := `
     package main
