db853bc0c6e95458de61e49c1fbbe965fc96aff0
wangyi6 <wangyi6@shopline.com>
2025-03-16 21:40:17 +0800

    1. 对接ast的expression

--- parser/expression/z3/expression.go
+++ parser/expression/z3/expression.go
@@ -34,45 +34,40 @@ func ExpressParam(param _struct.Parameter) *z3.AST {
 	case *expr.BasicLit:
 		return ExpressBasicLit(exprType)
 	default:
-		//elementList := []string{param.GetFormula()}
-		expression := &Z3Express{
-			//ElementList: elementList,
-			//Expr:        strings.Join(elementList, " "),
-		}
-		return []*Z3Express{expression}
+		panic("unknown expression type")
 	}
 }
 
-func ExpressTarget(param, targetParam _struct.Parameter) []*Z3Express {
-	expressionList := make([]*Z3Express, 0, 10)
-	eList := ExpressTargetParam(param, targetParam)
-	expressionList = append(expressionList, eList...)
-	return expressionList
-}
+//func ExpressTarget(param, targetParam _struct.Parameter) []*Z3Express {
+//	expressionList := make([]*Z3Express, 0, 10)
+//	eList := ExpressTargetParam(param, targetParam)
+//	expressionList = append(expressionList, eList...)
+//	return expressionList
+//}
 
 // ExpressTargetParam 将Binary、Unary解析为Expression，得到两个东西，一个是里面的Ident和func的引用，一个是最终得到的公式
-func ExpressTargetParam(param, targetParam _struct.Parameter) []*Z3Express {
-	switch exprType := param.(type) {
-	case *expr.Binary:
-		return ExpressBinary(exprType)
-	case *expr.Unary:
-		return ExpressUnary(exprType)
-	case *expr.Parent:
-		return ExpressParent(exprType)
-	case *expr.Ident:
-		return ExpressTargetIdent(exprType, targetParam)
-	case *expr.Selector:
-		return ExpressTargetSelector(exprType, targetParam)
-	case *expr.Call:
-		return ExpressTargetCall(exprType, targetParam)
-	case *expr.BasicLit:
-		return ExpressTargetBasicLit(exprType, targetParam)
-	default:
-		//elementList := []string{param.GetFormula()}
-		expression := &Z3Express{
-			//ElementList: elementList,
-			//Expr:        strings.Join(elementList, " "),
-		}
-		return []*Z3Express{expression}
-	}
-}
+//func ExpressTargetParam(param, targetParam _struct.Parameter) []*Z3Express {
+//	switch exprType := param.(type) {
+//	case *expr.Binary:
+//		return ExpressBinary(exprType)
+//	case *expr.Unary:
+//		return ExpressUnary(exprType)
+//	case *expr.Parent:
+//		return ExpressParent(exprType)
+//	case *expr.Ident:
+//		return ExpressTargetIdent(exprType, targetParam)
+//	case *expr.Selector:
+//		return ExpressTargetSelector(exprType, targetParam)
+//	case *expr.Call:
+//		return ExpressTargetCall(exprType, targetParam)
+//	case *expr.BasicLit:
+//		return ExpressTargetBasicLit(exprType, targetParam)
+//	default:
+//		//elementList := []string{param.GetFormula()}
+//		expression := &Z3Express{
+//			//ElementList: elementList,
+//			//Expr:        strings.Join(elementList, " "),
+//		}
+//		return []*Z3Express{expression}
+//	}
+//}
